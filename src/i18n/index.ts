0395d5d6fb09b274f891e94e91b4cca825bb1068
Jonathan Cruz <jonathan.cruz89@gmail.com>
2024-02-07 20:12:54 -0500

    Add i18n support and create Home page

---
+++ src/i18n/index.ts
@@ -0,0 +1,18 @@
+import spanish from "@/i18n/es.json";
+import english from "@/i18n/en.json";
+
+const LANGUAGES = {
+  SPANISH: "es",
+  ENGLISH: "en",
+};
+
+export const getI18N = ({
+  currentLocale = "es",
+}: {
+  currentLocale: string | undefined;
+}) => {
+  if (currentLocale === LANGUAGES.SPANISH) return spanish;
+  if (currentLocale === LANGUAGES.ENGLISH) return english;
+
+  return spanish;
+};
