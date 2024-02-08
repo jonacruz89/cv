a081ad1d6af792779b1d26ae6ba8b33122048d6d
Jonathan Cruz <jonathan.cruz89@gmail.com>
2024-02-07 21:06:49 -0500

    Update default locale and HTML lang attribute

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'es',
-    locales: ['es', 'ca'],
+    locales: ['es', 'en'],
     routing: {
       prefixDefaultLocale: false
     }
